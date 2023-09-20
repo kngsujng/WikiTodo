8dad393957441ca23a7f6f7597da37d0d95ea896
강수정 <kngsujng@naver.com>
2023-09-20 23:49:29 +0900

    Feat: 할 일 추가하면 useState를 통해 할 일 리스트 저장

--- src/Components/AddTodo/AddTodo.jsx
+++ src/Components/AddTodo/AddTodo.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './AddTodo.style';
 
+// 1. input창에 입력한 todo state로 관리 및 저장
+// 2. button 클릭시, todos 배열 state에 추가
+//		-
+
 export default function AddTodo() {
+	let [todoList, setTodoList] = useState([]);
+	let [newTodo, setNewTodo] = useState('');
 	return (
 		<>
 			<S.Wrapper>
@@ -10,8 +16,20 @@ export default function AddTodo() {
 					type="text"
 					placeholder="Input your to-do!"
 					id="inp_todo"
+					value={newTodo}
+					onChange={(e) => setNewTodo(e.target.value)}
 				/>
-				<button type="button">add</button>
+				<button
+					type="button"
+					onClick={(e) => {
+						e.preventDefault();
+						let todoItems = [...todoList]; // todoList 복사
+						setTodoList([...todoItems, newTodo]);
+						setNewTodo('');
+					}}
+				>
+					add
+				</button>
 			</S.Wrapper>
 		</>
 	);
