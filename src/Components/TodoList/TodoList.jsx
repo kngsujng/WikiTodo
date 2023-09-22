84eeba055cceec067d3ccc41412341fa3d94afa6
강수정 <kngsujng@naver.com>
2023-09-22 14:37:19 +0900

    :Fix: 다른 할 일 체크하면, 또 다른 할 일 체크가 없어지는 문제 해결

--- src/Components/TodoList/TodoList.jsx
+++ src/Components/TodoList/TodoList.jsx
@@ -4,7 +4,10 @@ import * as S from './TodoList.style';
 import { FaRegTrashAlt } from 'react-icons/fa';
 
 export default function TodoList({ todoList, setTodoList }) {
-	const [completed, setCompleted] = useState({ content: '', isChecked: false });
+	const [completed, setCompleted] = useState([]);
+	useEffect(() => {
+		console.log(completed);
+	}, [completed]);
 	return (
 		<>
 			<S.Wrapper>
@@ -16,21 +19,26 @@ export default function TodoList({ todoList, setTodoList }) {
 								<input
 									type="checkbox"
 									value={v}
-									onChange={(e) => {
-										setCompleted((prev) => ({
+									// checked={completed[i]?.isChecked}
+									onClick={(e) => {
+										//  ---> 여기서부터 여기까지 정상 작동<----
+										setCompleted((prev) => [
 											...prev,
-											content: e.target.value,
-											isChecked: e.target.checked,
-										}));
+											{
+												content: e.target.value,
+												isChecked: e.target.checked,
+											},
+										]);
+										//  ---> 여기서부터 여기까지 정상작동<----//
 									}}
 								/>
 								<p
 									className="todoItem"
-									style={
-										completed.isChecked && completed.content === v
-											? { color: '#bdbdbd', textDecoration: 'line-through' }
-											: null
-									}
+									// style={
+									// 	completed.isChecked && completed.content === v
+									// 		? { color: '#bdbdbd', textDecoration: 'line-through' }
+									// 		: null
+									// }
 								>
 									{v}
 								</p>
